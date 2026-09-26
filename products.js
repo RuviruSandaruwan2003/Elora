fb8f8b90a1e45317f96fa251dff07776c73577cb
RuviruSandaruwan2003 <ruvirusandaruwan63@gmail.com>
2026-09-26 23:42:41 +0530

    Update products.js

--- products.js
+++ products.js
@@ -6,16 +6,16 @@
    ========================================================================== */
 
 const ELORA_PRODUCTS = [
-  { id:'trolley',   name:'Elora Classic Trolley',  category:'Luggage', price:42000, image:'1.png' },
-  { id:'cabin',     name:'Elora Cabin Luggage',    category:'Luggage', price:34000, image:'2.png' },
-  { id:'backpack',  name:'Elora Travel Backpack',  category:'Luggage', price:18900, image:'3.png' },
-  { id:'duffel',    name:'Elora Travel Duffel',    category:'Luggage', price:38500, image:'4.png' },
-  { id:'weekender', name:'Elora Weekender Bag',    category:'Luggage', price:24500, image:'5.png' },
+  { id:'trolley',   name:'Elora Classic Trolley',  category:'Luggage', price:42000, image:'T1.jpeg' },
+  { id:'cabin',     name:'Elora Cabin Luggage',    category:'Luggage', price:34000, image:'T2.jpeg' },
+
+   
   { id:'loafer',    name:'Chunky Shoes - Clean White',   category:'Shoes',   price:6990, image:'S1.jpeg', sizes:[36,37,38,39,40] },
   { id:'sneaker',   name:'Chunky Shoes - Soft Pink',  category:'Shoes',   price:6990, image:'S2.jpeg', sizes:[36,37,38,39,40] },
   { id:'oxford',    name:'Air shoes - Green',    category:'Shoes',   price:4500, image:'S3.jpeg', sizes:[42,43,44] },
    { id:'oxford',    name:'Air shoes - Black',    category:'Shoes',   price:4500, image:'S4.jpeg', sizes:[42,43,44] },
-     { id:'oxford',    name:'Air shoes - Orange',    category:'Shoes',   price:4500, image:'S3.jpeg', sizes:[42,43,44] },
+     { id:'oxford',    name:'Air shoes - Orange',    category:'Shoes',   price:4500, image:'S5.jpeg', sizes:[42,43,44] },
+    { id:'oxford',    name:'Office wear -Black',    category:'Shoes',   price:8000, image:'S6.jpeg', sizes:[42,43,44] },
 ];
 
 function eloraFormatPrice(n){
